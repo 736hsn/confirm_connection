5e06b1c579132eb17a32bc6148f679cc6b46d741
Hassan Al-Basry <88793951+736hsn@users.noreply.github.com>
2022-12-12 13:48:01 +0300

    add description

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 ## 0.0.1
-
-* TODO: Describe initial release.
+ 
+Checking Network connection State on Android and iOS
--- lib/confirm_connection_network.dart
+++ lib/confirm_connection_network.dart
@@ -1,7 +1,23 @@
 library confirm_connection_network;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:async';
+import 'dart:io';
+
+class ConfirmConnection {
+  static Future<bool> confirmConnection() async {
+    /// Here, the result works like a 'ping' at the address below
+    try {
+      /// It's used 'google.com' because it has a global
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result.first.rawAddress.isNotEmpty) {
+        /// Here it's returned 'true', so the response of 'lookup' was successful
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      /// Here it's returned 'false', so there is not connection
+      return false;
+    }
+  }
 }
